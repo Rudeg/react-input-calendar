756ece172afa8c31427ad6294458f87ae9769082
Ed Lott <elott@cappex.com>
2017-06-23 12:50:40 -0500

    Test

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,4 +5,13 @@ import '../../style/index.css'
 
 storiesOf('Calendar', module).add('default view', () => {
   return <Calendar format="DD/MM/YYYY" date="01/01/2016" />
+}).add('Save on close', () => {
+  const saved = [''];
+  return <div>
+    <Calendar format="DD/MM/YYYY" closeOnSelect={true} onChange={(x)=> {
+      saved[0] = x;
+    }}/>
+    <label>Saved:</label>
+    <p>{saved[0]}</p>
+  </div>
 })
