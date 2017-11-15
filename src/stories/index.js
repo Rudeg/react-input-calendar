9d7597688377687e4267987dc235d53852ee04ad
Sergii Rudenko <rudenko.sergey92@gmail.com>
2017-11-14 16:47:54 -0800

    update storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf } from '@storybook/react'
 import Calendar from '../index'
 import '../../style/index.css'
 
